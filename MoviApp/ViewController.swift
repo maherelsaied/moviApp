9609bcf59c0e964af8d4fa4cbac7b9341d2d6a2a
maherelsaied <maherelsaied142@gmail.com>
2019-10-14 05:33:19 +0200

    Initial Commit

---
+++ MoviApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviApp
+//
+//  Created by Maher on 10/14/19.
+//  Copyright © 2019 Maher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
